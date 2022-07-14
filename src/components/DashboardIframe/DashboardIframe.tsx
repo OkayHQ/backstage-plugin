2851178352c74f04b2bd99c512214f18270a2716
Sarah Wada <sarah@okayhq.com>
2022-07-13 23:38:00 -0700

    update readme and update login

--- src/components/DashboardIframe/DashboardIframe.tsx
+++ src/components/DashboardIframe/DashboardIframe.tsx
@@ -14,13 +14,14 @@
  * limitations under the License.
  */
 import React from 'react';
-import { Card, CardContent } from '@material-ui/core';
-import { useApi } from '@backstage/core-plugin-api';
+import { Card, CardContent, Typography, Button } from '@material-ui/core';
+import { configApiRef, useApi } from '@backstage/core-plugin-api';
 // eslint-disable-next-line
 import { useAsync } from 'react-use';
 import { Progress } from '@backstage/core-components';
 import Alert from '@material-ui/lab/Alert';
 import { okayBackendPluginApiRef } from '../../api/OkayBackendPluginApi';
+
 
 const DashboardIframeCard = ({ dashboard }: { dashboard: {url: string} }) => {
   return (
@@ -32,23 +33,73 @@ const DashboardIframeCard = ({ dashboard }: { dashboard: {url: string} }) => {
           width="100%"
           height={800}
           frameBorder="0"
+          referrerPolicy="strict-origin-when-cross-origin"
         />
       </CardContent>
     </Card>
-  );
+  )
 };
 
 export const DashboardIframe = (props: {uuid: string}) => {
+  // Add event listener to indicate that we're on the login page (no login cookies). Because we don't
+  // want to actually support the login page in an iframe, which would be necessary as part of the login
+  // redirect flow, instead we direct users to open a new tab and log into Okay. This will set their
+  // browser cookies and authenticate them for the iframe request.
+  const config = useApi(configApiRef);
+  const domain = config.getOptionalString('okay.iframe.domain');
+
+  const [login, setLogin] = React.useState({ 
+    renderLogin: false,
+    refreshPage: false
+  })
+
+  window.addEventListener('message', (event) => {
+    if (event.origin === domain && event.type === 'message' && event.data === 'login_page') {
+      setLogin({ renderLogin: true, refreshPage: false })
+    } else {
+      // postMessage sent from different origin than the iframe, do nothing
+    }
+  }, false);
+
+  
   const okayApi = useApi(okayBackendPluginApiRef);
-const { value, loading, error } = useAsync(async () => await okayApi.iframeUrl(props.uuid));
+  const { value, loading, error } = useAsync(async () => await okayApi.iframeUrl(props.uuid));
 
   if (loading) {
     return <Progress />;
   } else if (error || !value) {
     return <Alert severity="error">{error ? error.message : 'Could not fetch iframe URL'}</Alert>;
+  } else if (login.renderLogin) {
+    return (
+      <Card>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Okay Dashboard
+          </Typography>
+          <Typography gutterBottom variant="body2" color="textSecondary">
+            Log in to Okay to see your dashboard
+          </Typography>
+          <Button size="small" variant="contained" onClick={() => {
+              setLogin({ renderLogin: false, refreshPage: true });
+              window.open(`${domain}`, '_blank');
+          }}>Take me to Okay</Button>
+        </CardContent>
+      </Card>
+    )
+  } else if (login.refreshPage) {
+    return (
+      <Card>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Okay Dashboard
+          </Typography>
+          <Typography gutterBottom variant="body2" color="textSecondary">
+            Please refresh the page to see your dashboard!
+          </Typography>
+        </CardContent>
+      </Card>
+    )
   }
 
-  return (
-        <DashboardIframeCard dashboard={ {url: value.url }} />
-      );
+  return <DashboardIframeCard dashboard={ { url: value.url }} />;
 };
